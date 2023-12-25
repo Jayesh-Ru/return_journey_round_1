e6612291250d311c56f00bc98539d741bd30e484
Jayesh-Ru <jayesh.khairnar@vphrase.com>
2023-12-25 23:51:33 +0530

    remove redundant code

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ const app = require('./src/app');
 import * as dotenv from 'dotenv'
 // dotenv.config()
 
-console.log('abcd-->',process.env.NODE_ENV)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
